3a3bd8a23c3d63e28bb4bce85a2bb0ce39df6906
Diego <hidal8909ins@gmail.com>
2025-06-22 18:23:42 -0500

    new class dataReader

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "dataReader.hh"
 
-int
-main() {
-    std::cout << "Hello World!\n";
+int main()
+{
+    
+    //DataReader test;
+    //std::cout << test.read_data("C:\\Users\\diego\\OneDrive\\Escritorio\\data_test.csv",
+    //                            "C:\\Users\\diego\\OneDrive\\Escritorio\\meta_data.csv") << "\n";
 }
